fe16c03bd54772ead84ec7d2c0789380837799cd
fernando <fernandoclr4@gmail.com>
2024-08-27 23:21:19 -0500

    primer archivo

---
+++ 2.3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int c{0}, thisIsVariable{0}, q76354{0};
+
+    cout<<"ingresa un numero entero: "; 
+
+    int a;
+
+    cin>>a;
+    cout<<a+10<<endl;
+
+    if (a>15)
+
+       cout<<"el nuevo valor es: "<<a-5<<endl;
+
+    else
+       cout<<"sigue siendo: "<<a<<endl;
+
+
+    cout<<"This is a C++ program\n";
+    cout<<"This is a C++\nprogram\n";
+    cout<<"This\nis\na\nC++\nprogram\n";
+    cout<<"This\tis\ta\tC++\tprogram\n";   
+
+
+
+
+
+
+
+}
